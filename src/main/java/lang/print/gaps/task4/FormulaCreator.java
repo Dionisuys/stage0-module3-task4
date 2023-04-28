8cd9a9c14db752628c39c3d36bc9a31534cd9c0e
Dzianis Panasiuk <dzianispanasiuk@gmail.com>
2023-04-28 23:27:27 +0300

    updated 3 classes on the Primitives

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,14 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        byte nine = 9;
+        char a = 'a';
+        byte five = 5;
+        char b = 'b';
+        byte two = 2;
+        byte seven = 7;
+        byte four = 4;
+        double formula = (nine * a * a - five * b + two + a - seven) * ((double) (a + b - four * a + b) / (two));
+        System.out.println(formula);
     }
 }
